ecb10c2ce693d5499222349a15958fa1a54e760c
Pedro Ferreira <pedrobf@student.dei.uc.pt>
2025-09-25 21:09:12 +0100

    implementação do cálculo do módulo de aceleração e plot do resultado

--- mainActivity.py
+++ mainActivity.py
@@ -1,8 +1,8 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 def descarregar_dados(array):
-    
     for i in range(0, 15):
         caminho = 'samples/FORTH_TRACE_DATASET-master/FORTH_TRACE_DATASET-master/part'
         caminho = f"{caminho}{i}/part{i}dev"
@@ -19,17 +19,50 @@ def descarregar_dados(array):
             array_aux.append(amostras)
         
         array.append(array_aux)
-    
     return np.array(array, dtype=object) # Uso de dtype=object pelo fato de o nº de linhas variar 
+
+def calcular_modulo_aceleracao(dados):
+    modulos = [[] for _ in range(16)]
+    for j in range(0, 15):
+        for k in range(0, 5):
+            for l in range(0, len(dados[j][k])):
+                row = dados[j][k][l]
+                try:
+                    atividade = int(row[11])
+                    if 1 <= atividade <= 16:
+                        acc_x = float(row[1])
+                        acc_y = float(row[2])
+                        acc_z = float(row[3])
+                        modulo = np.linalg.norm([acc_x, acc_y, acc_z])
+                        modulos[atividade-1].append(modulo)
+                except (ValueError, IndexError):
+                    continue  # pula linhas mal formatadas
+    return np.array(modulos, dtype=object)
+
+def plot_atividade(modulos): 
+    plt.boxplot([modulos[0], modulos[1], modulos[2],
+                 modulos[3], modulos[4], modulos[5],
+                 modulos[6], modulos[7], modulos[8],
+                 modulos[9], modulos[10], modulos[11], 
+                 modulos[12], modulos[13], modulos[14], 
+                 modulos[15]], 
+                 labels=["1", "2", "3", 
+                         "4", "5", "6", 
+                         "7", "8", "9", 
+                         "10", "11", "12",
+                         "13", "14", "15",
+                         "16"])
+    plt.title("Boxplot de múltiplos grupos")
+    plt.ylabel("Valores de aceleração")
+    plt.show()
+
 
 if __name__=="__main__":
     array = []
     dados = descarregar_dados(array)
-    print(len(array[0][0]))
-
-
-
-
+    modulos = calcular_modulo_aceleracao(dados)
+    plot_atividade(modulos)
+    
 
 #Coluna 1: Device ID
 #Coluna 2: accelerometer x
