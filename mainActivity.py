e5375e03c45ffa24a7a181efb0f57aff091cd5d9
Pedro Bras Ferreira <dibasferr51@gmail.com>
2025-09-25 18:55:58 +0100

    carregamento dos dados em um array NumPy

--- mainActivity.py
+++ mainActivity.py
@@ -1,30 +1,32 @@
 import csv
 import numpy as np
 
-def descarregar_dados():
-    array = []
+def descarregar_dados(array):
     
     for i in range(0, 15):
-        caminho = 'samples\FORTH_TRACE_DATASET-master\FORTH_TRACE_DATASET-master\part'
-        caminho = f"{caminho}{i}\part{i}dev"
+        caminho = 'samples/FORTH_TRACE_DATASET-master/FORTH_TRACE_DATASET-master/part'
+        caminho = f"{caminho}{i}/part{i}dev"
 
         array_aux = []
 
         for j in range(1, 6):
+            amostras = []
             caminho2 = f"{caminho}{j}.csv"
             with open(caminho2, newline='') as csvfile:
                 spamreader = csv.reader(csvfile, delimiter = ',')
                 for row in spamreader:
-                    array_aux.append(row)
+                    amostras.append(row)
+            array_aux.append(amostras)
         
         array.append(array_aux)
-
-    #array = np.array(array)
-    print(array)
-
+    
+    return np.array(array, dtype=object) # Uso de dtype=object pelo fato de o nº de linhas variar 
 
 if __name__=="__main__":
-    descarregar_dados()
+    array = []
+    dados = descarregar_dados(array)
+    print(len(array[0][0]))
+
 
 
 
